8d192a144753439f02a9cf41dad4e21a627cd494
pjh2004 <pjh2004@gmail.com>
2023-11-29 16:25:08 -0500

    updated cube_gen to be dynamic
    Needs portmapping in top to work. Will not compile!

--- source/impl_1/cube_gen.vhd
+++ source/impl_1/cube_gen.vhd
@@ -6,6 +6,7 @@ entity cube_gen is
 	port(
 		row : in unsigned(9 downto 0);
 		col : in unsigned(9 downto 0);
+		cube_bot : in unsigned(9 downto 0) := 229;
 		rgb : out std_logic_vector(5 downto 0);
 		valid : in std_logic
 	);
@@ -13,10 +14,17 @@ end cube_gen;
 
 architecture synth of cube_gen is
 
+signal ground_top : unsigned(9 downto 0) := 230;
+signal ground_bot : unsigned(9 downto 0) := 230 + 10; -- 10 pixels wide
+signal cube_top : unsigned(9 downto 0);
+
 begin
-	rgb <= "000000" when (valid = '1' and row < 229) else 
-		"110000" when (valid = '1' and row >= 229 and row <= 249 and col >= 309 and col <= 329) else
-		"000000" when (valid = '1' and row > 249) else
+	--Cube height of 20
+	cube_top <= cube_bot + 20;
+
+	rgb <= "000000" when (valid = '1' and row < cube_bot) else 
+		"110000" when (valid = '1' and row >= cube_bot and row <= cube_top and col >= 209 and col <= 229) else -- 209 and 229 are off center to the left of screen
+		"001100" when (valid = '1' and row >= ground_top and row <= ground_bot) else
 		"000000";
 	
 end;
